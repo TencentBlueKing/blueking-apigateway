262af26152577eb3c5db453b36646fbb148e82ec
Han-Ya-Jun <1581532052@qq.com>
2023-07-13 20:01:44 +0800

    fix: fix stage

--- src/core-api/pkg/service/publish_event_test.go
+++ src/core-api/pkg/service/publish_event_test.go
@@ -125,7 +125,7 @@ var _ = Describe("PublishEventService", func() {
 			)
 			err := svc.Report(ctx, event)
 			assert.Error(GinkgoT(), err)
-			assert.Contains(GinkgoT(), err.Error(), "get stage[test] info failed")
+			assert.Contains(GinkgoT(), err.Error(), "get Stage[test] info failed")
 		})
 
 		It("error: duplicate report", func() {
