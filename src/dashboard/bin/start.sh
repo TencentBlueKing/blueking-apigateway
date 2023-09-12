1b65375520f5f7127ba4f5ffcc230c253881968f
alex-smile <443677891@qq.com>
2023-09-12 19:48:03 +0800

    revert dashboard start.sh

--- src/dashboard/bin/start.sh
+++ src/dashboard/bin/start.sh
@@ -10,5 +10,5 @@ fi
 
 python manage.py collectstatic --no-input
 
-command="gunicorn wsgi --env prometheus_multiproc_dir=/tmp/ -k gevent -w 4 -b [::]:${PORT:-6000} --max-requests ${GUNICORN_MAX_REQUESTS:-1000} --max-requests-jitter ${GUNICORN_MAX_REQUESTS_JITTER:-0} --timeout ${GUNICORN_TIMEOUT:-60} --graceful-timeout ${GUNICORN_GRACEFUL_TIMEOUT:-30} --access-logfile - --error-logfile - --access-logformat '[%(h)s] %({request_id}i)s %(u)s %(t)s \"%(r)s\" %(s)s %(D)s %(b)s \"%(f)s\" \"%(a)s\"'"
+command="gunicorn wsgi --env prometheus_multiproc_dir=/tmp/ -k gevent -w 4 -b [::]:${PORT:-6000} --timeout 60 --max-requests 1000 --access-logfile - --error-logfile - --access-logformat '[%(h)s] %({request_id}i)s %(u)s %(t)s \"%(r)s\" %(s)s %(D)s %(b)s \"%(f)s\" \"%(a)s\"'"
 exec bash -c "$command"
