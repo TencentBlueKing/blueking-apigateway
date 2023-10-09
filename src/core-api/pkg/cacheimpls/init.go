63004dced5a7606ee8023cc45515a733563a101c
hanyajun <1581532052@qq.com>
2023-10-09 11:56:27 +0800

    core api publish event add cache 1.12 (#277)
    * feat: publish event add cache
    
    * feat: fix cr

--- src/core-api/pkg/cacheimpls/init.go
+++ src/core-api/pkg/cacheimpls/init.go
@@ -24,6 +24,7 @@ import (
 
 	"github.com/TencentBlueKing/gopkg/cache/memory"
 	"github.com/TencentBlueKing/gopkg/cache/memory/backend"
+	gocache "github.com/patrickmn/go-cache"
 )
 
 // DisableCache will turn off the cache, all query will go to db
@@ -84,6 +85,9 @@ var (
 		12*time.Hour,
 		newRandomDuration(30),
 	)
+
+	// event_cache: gateway_id:stage_id:publish_id:step:status
+	publishEventCache = gocache.New(10*time.Minute, 15*time.Minute)
 
 	// NOTE: 权限-缓存的变更来源
 	// part1: 权限本身的操作
