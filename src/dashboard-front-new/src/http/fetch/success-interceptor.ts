657b4f404f6d250764e2d1ae3f611f9cf362a9cb
polo <826770122@qq.com>
2023-10-11 19:42:11 +0800

    feat: add error message

--- src/dashboard-front-new/src/http/fetch/success-interceptor.ts
+++ src/dashboard-front-new/src/http/fetch/success-interceptor.ts
@@ -1,39 +1,25 @@
 import type { IFetchConfig } from './index';
-import RequestError from './request-error';
 
-const { BK_LOGIN_URL } = window;
+interface IErrorInfo {
+  status?: Number;
+  error?: any
+}
 
 // 请求成功执行拦截器
 export default async (response: any, config: IFetchConfig) => {
-  console.log('response', response);
-  const {
-    code = response.code,
-    data,
-    message = response.statusText,
-  } = await response[config.responseType]();
+  const responseInfo =  await response[config.responseType]();
   if (response.ok) {
-    // 对应 HTTP 请求的状态码 200 到 299
-    // 校验接口返回的数据，status 为 0 表示业务成功
-    // switch (response.status) {
-    //   // 接口请求成功
-    //   case 0:
-    //     return Promise.resolve(data);
-    //   // 后端业务处理报错
-    //   default:
-    //     throw new RequestError(code, message || '系统错误', data);
-    // }
     const reg = RegExp(/20/);
     // 包含20x 代表请求成功
     if (reg.test(response.status)) {
-      return Promise.resolve(data);
+      return Promise.resolve(responseInfo.data);
     }
-    throw new RequestError(code, message || '系统错误', data);
-  } else {
-    // 处理 http 非 200 异常
-    if (response.status === 401) {
-      window.location.href = `${BK_LOGIN_URL}/?c_url=${window.location.href}`;
-    }
-    console.log(111, response);
-    throw new RequestError(code || -1, message || '系统错误', data);
+    return Promise.reject(response);
   }
+  // 处理 http 非 200 异常
+  const errorInfo: IErrorInfo  = {
+    status: response.status,
+    error: responseInfo.error,
+  };
+  return Promise.reject(errorInfo);
 };
