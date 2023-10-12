8fe51119f521b6d732c69ea610ffe27e91b66508
polo <826770122@qq.com>
2023-10-12 21:34:43 +0800

    feat: 添加页面路由相关

--- src/dashboard-front-new/src/router/index.ts
+++ src/dashboard-front-new/src/router/index.ts
@@ -1,6 +1,7 @@
 import {
   createRouter,
   createWebHistory,
+  RouteRecordRaw,
 } from 'vue-router';
 
 const Home = () => import(/* webpackChunkName: "Home" */ '@/views/home.vue');
@@ -8,33 +9,47 @@ const ApigwMain = () => import(/* webpackChunkName: 'apigw-main'*/'@/views/main.
 const ApigwResource = () => import(/* webpackChunkName: 'apigw-main'*/'@/views/resource/index.vue');
 const ApigwDoc = () => import(/* webpackChunkName: 'apigw-doc'*/'@/views/components/doc/index.vue');
 
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home,
+    meta: {
+      isMenu: false,   // 是否作为侧边栏菜单
+    },
+  },
+  {
+    path: '/',
+    name: 'apigwMain',
+    component: ApigwMain,
+    meta: {
+      title: '资源管理',
+      matchRoute: 'apigwMain',
+    },
+    children: [
+      {
+        path: '/:id/stage',
+        name: 'apigwDoc',
+        component: ApigwDoc,
+        meta: {
+          title: '环境管理',
+          matchRoute: 'apigwDoc',
+        },
+      },
+      {
+        path: '/:id/resource',
+        name: 'apigwResource',
+        component: ApigwResource,
+        meta: {
+          title: '资源配置',
+          matchRoute: 'apigwResource',
+        },
+      },
+    ],
+  },
+];
 export default createRouter({
   history: createWebHistory(window.SITE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-    },
-    {
-      path: '/apigw-api',
-      name: 'apigwDoc',
-      component: ApigwDoc,
-    },
-    {
-      path: '/',
-      component: ApigwMain,
-      children: [
-        {
-          path: '/:id/resource',
-          name: 'apigwResource',
-          component: ApigwResource,
-          meta: {
-            title: '资源管理',
-            matchRoute: 'apigwResource',
-          },
-        },
-      ],
-    },
-  ],
+  routes,
 });
