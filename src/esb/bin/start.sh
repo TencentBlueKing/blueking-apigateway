637f85dde60822736ceac7e31a0171fb7db55cff
alex-smile <443677891@qq.com>
2023-10-20 11:27:05 +0800

    update esb gunicorn param: set keep-alive to 0 (#300)

--- src/esb/bin/start.sh
+++ src/esb/bin/start.sh
@@ -8,5 +8,5 @@ if [ -f "${envfile}" ]; then
     set +a
 fi
 
-command="gunicorn wsgi --env prometheus_multiproc_dir=/tmp/ -k gevent -w 16 -b [::]:${PORT:-6010} --max-requests ${GUNICORN_MAX_REQUESTS:-10000} --max-requests-jitter ${GUNICORN_MAX_REQUESTS_JITTER:-2000} --timeout 600 --graceful-timeout ${GUNICORN_GRACEFUL_TIMEOUT:-30} --access-logfile - --error-logfile - --access-logformat '[%(h)s] %({request_id}i)s %(u)s %(t)s \"%(r)s\" %(s)s %(D)s %(b)s \"%(f)s\" \"%(a)s\"'"
+command="gunicorn wsgi --env prometheus_multiproc_dir=/tmp/ -k gevent -w ${GUNICORN_WORKERS:-16} -b [::]:${PORT:-6010} --max-requests ${GUNICORN_MAX_REQUESTS:-10000} --max-requests-jitter ${GUNICORN_MAX_REQUESTS_JITTER:-2000} --timeout 600 --graceful-timeout ${GUNICORN_GRACEFUL_TIMEOUT:-30} --keep-alive ${GUNICORN_KEEP_ALIVE:-0} --access-logfile - --error-logfile - --access-logformat '[%(h)s] %({request_id}i)s %(u)s %(t)s \"%(r)s\" %(s)s %(D)s %(b)s \"%(f)s\" \"%(a)s\"'"
 exec bash -c "$command"
