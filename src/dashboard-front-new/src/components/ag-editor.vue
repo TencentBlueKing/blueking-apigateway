194a8479a9097ef4ad5ea5e053017a962d427b44
polo <826770122@qq.com>
2023-11-06 17:17:33 +0800

    feat: add editor

---
+++ src/dashboard-front-new/src/components/ag-editor.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="codemirror">
+    <div id="monacoEditor" class="monaco-editor" ref="monacoEditor" :style="style"></div>
+  </div>
+</template>
+<script setup>
+// 引入vue模块
+import { ref, onMounted, toRefs, computed, watch, onBeforeMount } from 'vue';
+// 引入monaco编辑器
+import * as monaco from 'monaco-editor';
+
+const editor = ref(null); // 编辑器实例
+const monacoEditor = ref(null);
+// 定义从父组件接收的属性
+const props = defineProps({
+  value: { type: [String, Object, Array], default: () => 'yaml' },
+  language: { type: String, default: 'yaml' },
+  readOnly: { type: Boolean, default: false },
+  width: { type: [String, Number], default: '100%' },
+  height: { type: [String, Number], default: '100%' },
+});
+
+const { value, language, readOnly, width, height } = toRefs(props);
+
+// 挂载
+onMounted(() => {
+  console.log('value', value.value);
+  initEditor();
+});
+
+// 卸载
+onBeforeMount(() => {
+  editor.value?.dispose();
+  editor.value = null;
+});
+
+const style = computed(() => ({
+  width: typeof width.value === 'number' ? `${width.value}px` : width.value,
+  height: typeof height.value === 'number' ? `${height.value}px` : height.value,
+}));
+
+// 设置值
+const setValue = (value) => {
+  try {
+    if (!editor.value) return null;
+    return editor.value.setValue(value);
+  } catch (err) {
+    console.log('err', err);
+  }
+};
+
+// 非只读模式时，建议手动调用setValue方法，watch在双向绑定时会让编辑器抖动
+watch(value, () => {
+  if (readonly.value) {
+    setValue(value.value);
+  }
+});
+
+// 获取编辑器中的值
+const getValue = () => {
+  if (!editor.value) return '';
+  return editor.value.getValue();
+};
+
+// 初始化编辑器
+const initEditor = () => {
+  editor.value = monaco.editor.create(monacoEditor.value, {
+    value: value.value,
+    theme: 'vs-dark', // 主题
+    language: language.value,
+    folding: true, // 是否折叠
+    foldingHighlight: true, // 折叠等高线
+    foldingStrategy: 'indentation', // 折叠方式  auto | indentation
+    showFoldingControls: 'always', // 是否一直显示折叠 always | mouseover
+    disableLayerHinting: true, // 等宽优化
+    emptySelectionClipboard: false, // 空选择剪切板
+    selectionClipboard: false, // 选择剪切板
+    automaticLayout: true, // 自动布局
+    codeLens: false, // 代码镜头
+    scrollBeyondLastLine: false, // 滚动完最后一行后再滚动一屏幕
+    colorDecorators: true, // 颜色装饰器
+    accessibilitySupport: 'off', // 辅助功能支持  "auto" | "off" | "on"
+    lineNumbers: 'on', // 行号 取值： "on" | "off" | "relative" | "interval" | function
+    lineNumbersMinChars: 5, // 行号最小字符   number
+    readOnly: readOnly.value, // 是否只读  取值 true | false
+  });
+
+  editorMounted(); // 编辑器初始化后
+};
+
+const editorMounted = () => {
+  editor.value.onDidChangeModelContent((event) => {
+    const yamlValue = getValue();
+    emitChange(yamlValue, event);
+  });
+};
+
+const emitChange = (emitValue, event) => {
+  console.log('emitValue', emitValue);
+  ctx.emit('change', emitValue, event);
+  ctx.emit('input', emitValue, event);
+};
+
+defineExpose({
+  setValue,
+});
+
+</script>
+<style scoped>
+.codemirror,
+.monaco-editor {
+    width: 100%;
+    height: 100%;
+}
+</style>
--- src/dashboard-front-new/src/views/resource/import.vue
+++ src/dashboard-front-new/src/views/resource/import.vue
@@ -1,3 +1,74 @@
 <template>
-  <div>导入</div>
+  <div class="import-container p20">
+    <div class="import-header flex-row justify-content-between">
+      <div class="flex-row align-items-center">
+        <bk-button>
+          <i class="icon apigateway-icon icon-ag-add-small pr10"></i>
+          导入 Swagger 文件
+        </bk-button>
+        <span class="desc">（json /yaml 格式）</span>
+        <bk-form class="flex-row">
+          <bk-form-item class="mb0" :label-width="20">
+            <bk-checkbox>
+              生成资源文档
+            </bk-checkbox>
+          </bk-form-item>
+          <bk-form-item class="mb0" label="文档语言" :required="true" :label-width="120">
+            <bk-radio-group>
+              <bk-radio label="zh">中文文档</bk-radio>
+              <bk-radio label="en">英文文档</bk-radio>
+            </bk-radio-group>
+          </bk-form-item>
+        </bk-form>
+      </div>
+      <div class="flex-row align-items-center">
+        <bk-link theme="primary">
+          模板示例
+        </bk-link>
+        <bk-link theme="primary" class="pl10">
+          <i class="apigateway-icon icon-ag-info"></i>
+          Swagger 说明文档
+        </bk-link>
+      </div>
+    </div>
+
+    <div class="monacoEditor mt20">
+      <editor-monaco v-model="test" ref="resourceEditorRef" />
+    </div>
+  </div>
 </template>
+<script setup lang="ts">
+import editorMonaco from '@/components/ag-editor.vue';
+import {  ref } from 'vue';
+
+const test = ref('test111111');
+
+const resourceEditorRef: any = ref<InstanceType<typeof editorMonaco>>(); // 实例化
+
+setTimeout(() => {
+  // console.log(11111, resourceEditorRef.value.setValue('111'));
+  // resourceEditorRef.value.setValue(test.value);
+}, 1000);
+
+// nextTick(() => {
+//   resourceEditorRef
+// })
+
+</script>
+<style scoped lang="scss">
+.import-container{
+  .import-header{
+    .icon-ag-add-small{
+      font-size: 16px;
+    }
+    .desc{
+      font-size: 12px;
+      color: #979ba5;
+    }
+  }
+  .monacoEditor {
+    width: 100%;
+    height: calc(100vh - 240px);
+  }
+}
+</style>
