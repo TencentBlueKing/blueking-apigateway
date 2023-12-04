f9f4b1b84fcbeb4a045c7929eb5e209e9da1f221
Han-Ya-Jun <1581532052@qq.com>
2023-12-04 16:35:00 +0800

    fix: fix cr

--- src/dashboard/apigateway/apigateway/apis/web/sdk/views.py
+++ src/dashboard/apigateway/apigateway/apis/web/sdk/views.py
@@ -101,9 +101,7 @@ class GatewaySDKListCreateApi(generics.ListCreateAPIView):
                     operator=self.request.user.username,
                 )
             except exceptions.ResourcesIsEmpty:
-                raise error_codes.INTERNAL.format(
-                    _("网关下无符合条件(请求方法要求不能为Any)的资源，无法生成 SDK。"), replace=True
-                )
+                raise error_codes.INTERNAL.format(_("网关下无符合条件的资源，无法生成 SDK。"), replace=True)
             except exceptions.GenerateError:
                 raise error_codes.INTERNAL.format(_("网关 SDK 生成失败。"), replace=True)
             except exceptions.PackError:
