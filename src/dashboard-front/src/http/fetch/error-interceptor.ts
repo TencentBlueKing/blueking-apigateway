28b8c27d8ad90c7f9e7388437efedd5d64b63a2a
lhzzforever <zly2576885332@163.com>
2024-02-04 17:38:25 +0800

    feature: 增加无权限访问小窗口弹窗组件

--- src/dashboard-front/src/http/fetch/error-interceptor.ts
+++ src/dashboard-front/src/http/fetch/error-interceptor.ts
@@ -1,5 +1,6 @@
 import type { IFetchConfig } from './index';
 import { Message } from 'bkui-vue';
+import mitt from '@/common/event-bus';
 // import { showLoginModal } from '@/common/auth';
 const { BK_LOGIN_URL } = window;
 
@@ -8,7 +9,9 @@ export default (errorData: any, config: IFetchConfig) => {
   const {
     status,
     error,
+    response,
   } = errorData;
+  const loginPlainUrl = response?.data?.data?.login_plain_url;
   switch (status) {
     // 参数错误
     case 400:
@@ -16,7 +19,8 @@ export default (errorData: any, config: IFetchConfig) => {
       break;
     // 用户登录状态失效
     case 401:
-      window.location.href = `${BK_LOGIN_URL}/?c_url=${window.location.href}`;
+      mitt.emit('show-login-modal', loginPlainUrl || `${BK_LOGIN_URL}/?c_url=${window.location.href}`);
+      // window.location.href = `${BK_LOGIN_URL}/?c_url=${window.location.href}`;
       break;
   }
   // 全局捕获错误给出提示
