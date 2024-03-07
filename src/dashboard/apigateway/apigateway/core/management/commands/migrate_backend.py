d451c2e833a35e3bee6edc6f9dcd150c43c8d278
Timmy <zhu327@gmail.com>
2024-03-07 10:29:07 +0800

    fix: migrate gateway backend log print (#503)

--- src/dashboard/apigateway/apigateway/core/management/commands/migrate_backend.py
+++ src/dashboard/apigateway/apigateway/core/management/commands/migrate_backend.py
@@ -39,14 +39,14 @@ class Command(BaseCommand):
 
         paginator = Paginator(qs, 100)
         for i in paginator.page_range:
-            logger.info("migrate gateway count %s", (i + 1) * 100)
-
             for gateway in paginator.page(i):
                 self._handle_gateway(gateway)
 
         logger.info("finish migrate gateway backend")
 
     def _handle_gateway(self, gateway: Gateway):
+        logger.info("process gateway id: %d name: %s", gateway.id, gateway.name)
+
         # 创建默认backend
         default_backend, _ = Backend.objects.get_or_create(gateway=gateway, name=DEFAULT_BACKEND_NAME)
 
