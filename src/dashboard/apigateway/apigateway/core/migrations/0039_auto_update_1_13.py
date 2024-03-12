fc5a483d514e5ffa31f08102c42eaea247722f75
Han-Ya-Jun <1581532052@qq.com>
2024-03-12 20:13:10 +0800

    fix: fix bug

--- src/dashboard/apigateway/apigateway/core/migrations/0039_auto_update_1_13.py
+++ src/dashboard/apigateway/apigateway/core/migrations/0039_auto_update_1_13.py
@@ -8,10 +8,10 @@ def run_migrate_command(apps, schema_editor):
     call_command("migrate_access_strategy")
     call_command("migrate_access_strategy_user_verified")
     call_command("migrate_backend")
+    call_command("delete_celery_task", task_name="apigateway.controller.tasks.syncing.release_updated_check")
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('plugin', '0012_pluginbinding_source'),
         ('core', '0038_auto_20231023_1526'),
