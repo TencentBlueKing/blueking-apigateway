503374987348bfc200acba35d966b47dc2adda2f
polo <826770122@qq.com>
2024-03-20 16:51:31 +0800

    feat: add config

--- src/dashboard-front/src/components/product-info.vue
+++ src/dashboard-front/src/components/product-info.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n';
+import { useGetGlobalProperties } from '@/hooks';
 
 const { t } = useI18n();
 
+const globalProperties = useGetGlobalProperties();
+const { GLOBAL_CONFIG } = globalProperties;
 // 跳转去外部页面
 const handleToExternal = () => {
 
@@ -20,18 +23,18 @@ const handleToExternal = () => {
         <span class="icon apigateway-icon icon-ag-help-document-fill"></span>
     </div>
     <template #content>
-      <div
-        class="info-item"
-        @click="handleToExternal"
-      >{{t('产品文档')}}</div>
-      <div
-        class="info-item"
-        @click="handleToExternal"
-      >{{t('问题反馈')}}</div>
-      <div
-        class="info-item"
-        @click="handleToExternal"
-      >{{t('开源社区')}}</div>
+      <bk-link class="info-item" :href="GLOBAL_CONFIG.HELPER.href" target="_blank"
+        v-if="GLOBAL_CONFIG.HELPER.href && GLOBAL_CONFIG.HELPER.name">
+        {{ t(GLOBAL_CONFIG.HELPER.name) }}
+      </bk-link>
+      <!-- v-if="GLOBAL_CONFIG.BK_FEED_BACK_LINK" -->
+      <bk-link class="info-item" :href="GLOBAL_CONFIG.BK_FEED_BACK_LINK" target="_blank">
+        {{ t('问题反馈') }}
+      </bk-link>
+      <!-- v-if="GLOBAL_CONFIG.MARKER" -->
+      <bk-link class="info-item" :href="GLOBAL_CONFIG.MARKER" target="_blank">
+        {{ t('开源社区') }}
+      </bk-link>
     </template>
   </bk-popover>
 </template>
@@ -39,17 +42,17 @@ const handleToExternal = () => {
 <style lang="scss" scoped>
 .info-icon {
   cursor: pointer;
-  margin-right: 20px;
+  margin-right: 30px;
 }
 .info-icon:hover {
   color: #fff;
 }
 .info-item {
-  text-align: center;
-  cursor: pointer;
+  display: block;
   color: #63656E;
   padding: 8px 15px;
   margin-top: 5px;
+  font-size: 12px;
 }
 .info-item:hover {
   background: #f3f6f9;
