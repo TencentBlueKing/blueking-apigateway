650df95ad639ae6eaaae7a05a82ec089d39a15e6
wklken <wklken@gmail.com>
2024-06-17 17:11:22 +0800

    fix(test/sync): fix run.sh fail (#701)

--- test/sync/run.sh
+++ test/sync/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CURRENT_DIR=$(PWD)
+CURRENT_DIR=$(pwd)
 
 if [[ -z $1 ]]; then
     echo "please set the arg of domain"
@@ -16,6 +16,7 @@ docker run --rm \
     -e BK_API_URL_TMPL="http://bkapi.${DOMAIN}/api/{api_name}" \
     -e BK_APP_CODE=bk_apigateway \
     -e BK_APP_SECRET=358627d8-d3e8-4522-8f16-b5530776bbb8 \
+    --network=host \
     hub.bktencent.com/blueking/apigw-manager:3.0.3
 
 
@@ -26,6 +27,7 @@ docker run --rm \
     -e BK_API_URL_TMPL="http://bkapi.${DOMAIN}/api/{api_name}" \
     -e BK_APP_CODE=bk_apigateway \
     -e BK_APP_SECRET=358627d8-d3e8-4522-8f16-b5530776bbb8 \
+    --network=host \
     hub.bktencent.com/blueking/apigw-manager:3.0.3
 
 # sync smoke stage2
@@ -35,5 +37,6 @@ docker run \
     -e BK_API_URL_TMPL="http://bkapi.${DOMAIN}/api/{api_name}" \
     -e BK_APP_CODE=bk_apigateway \
     -e BK_APP_SECRET=358627d8-d3e8-4522-8f16-b5530776bbb8 \
+    --network=host \
     hub.bktencent.com/blueking/apigw-manager:3.0.3 \
     bash -c  'source /apigw-manager/bin/functions.sh && call_definition_command_or_exit sync_apigw_stage /data/definition.yaml --gateway-name=smoke --namespace="stage2" && call_definition_command_or_exit create_version_and_release_apigw /data/definition.yaml'
