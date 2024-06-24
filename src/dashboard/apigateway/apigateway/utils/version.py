24f7d82ec8364b8b58393693a2c840d0a8198cdf
Lawrence <138839709+Lawrence-lkq@users.noreply.github.com>
2024-06-24 22:34:09 +0800

    Update version.py

--- src/dashboard/apigateway/apigateway/utils/version.py
+++ src/dashboard/apigateway/apigateway/utils/version.py
@@ -15,7 +15,7 @@
 # We undertake not to change the open source license (MIT license) applicable
 # to the current version of the project delivered to anyone in the future.
 #
-
+import re
 from typing import List
 
 from packaging import version
