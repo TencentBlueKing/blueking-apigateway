2da15a1f56a393d68728b1adff572b6ee97330fa
Lawrence <138839709+Lawrence-lkq@users.noreply.github.com>
2024-06-25 10:55:38 +0800

    Update version.py

--- src/dashboard/apigateway/apigateway/utils/version.py
+++ src/dashboard/apigateway/apigateway/utils/version.py
@@ -52,25 +52,11 @@ def max_version(versions: List[str]) -> str:
 
 
 def get_next_version(current_version: str) -> str:
-    match = re.match(r"^(\d+)\.(\d+)\.(\d+)(?:-([a-zA-Z0-9.+-]+))?(?:\+([a-zA-Z0-9.+-]+))?$", current_version)
-    if not match:
-        return "0.0.1"
-
-        # 提取主版本号、次版本号和补丁版本号
-    major, minor, patch = int(match.group(1)), int(match.group(2)), int(match.group(3))
-    pre_release = match.group(4)  # 预发布标签
-    build_metadata = match.group(5)  # 构建元数据
-
-    # 根据不同的类型增加版本号
-    patch += 1
-
-    # 构造新的版本号字符串，并保留后缀（如果需要）
-    new_version = f"{major}.{minor}.{patch}"
-    if pre_release:
-        new_version += f"-{pre_release}"
-    if build_metadata:
-        new_version += f"+{build_metadata}"
-    return new_version
+    try:
+        version = parse(current_version)
+        return str(Version(f"{version.major}.{version.minor}.{version.micro + 1}"))
+    except Exception:
+        return ""
 
 
 def is_version1_greater_than_version2(version1: str, version2: str) -> bool:
