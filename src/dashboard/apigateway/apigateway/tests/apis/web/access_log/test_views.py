67f424f71d39286fe9b002afe2b133396d706efb
Lawrence <138839709+Lawrence-lkq@users.noreply.github.com>
2024-07-23 11:45:11 +0800

    feat:logs export (#758)

--- src/dashboard/apigateway/apigateway/tests/apis/web/access_log/test_views.py
+++ src/dashboard/apigateway/apigateway/tests/apis/web/access_log/test_views.py
@@ -15,9 +15,13 @@
 # We undertake not to change the open source license (MIT license) applicable
 # to the current version of the project delivered to anyone in the future.
 #
+import csv
+from io import StringIO
+
 import pytest
 
 from apigateway.biz.access_log.constants import ES_LOG_FIELDS
+from apigateway.utils.time import format
 
 pytestmark = pytest.mark.django_db
 
@@ -133,3 +137,42 @@ class TestLogLinkRetrieveApi:
 
         assert response.status_code == 200
         assert result["data"]["link"]
+
+
+class TestLogExportApi:
+    def test_get(self, mocker, request_view, fake_stage):
+        mock_time_start = 1720606081
+        mock_time_end = 1721210881
+        mocker.patch(
+            "apigateway.apis.web.access_log.views.LogSearchClient.search_logs",
+            return_value=(3, [{"a": 1}, {"a": 2}, {"a": 3}]),
+        )
+
+        fake_gateway = fake_stage.gateway
+
+        response = request_view(
+            "GET",
+            "access_log.export",
+            path_params={"gateway_id": fake_gateway.id},
+            gateway=fake_gateway,
+            data={
+                "stage_id": fake_stage.id,
+                "time_range": 300,
+                "offset": 0,
+                "time_start": mock_time_start,
+                "time_end": mock_time_end,
+                "limit": 3,
+                "query": "api_id: 2",
+            },
+        )
+        time_start = format(mock_time_start, "%Y%m%d%H%M%S")
+        time_end = format(mock_time_end, "%Y%m%d%H%M%S")
+        assert response.status_code == 200
+        assert response.headers["Content-Type"] == "text/csv; charset=utf-8"
+        assert (
+            response["Content-Disposition"]
+            == f'attachment; filename="{fake_gateway.name}-{time_start}-{time_end}-logs.csv"'
+        )
+        reader = csv.DictReader(StringIO(response.content.decode("utf-8")))
+        log_count = sum(1 for _ in reader)
+        assert log_count == 3
