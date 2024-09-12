6ac152f4a8b0cc11d2a13131fc0d88288fb19e2a
hanyajun <1581532052@qq.com>
2024-09-12 17:44:34 +0800

    fix(api/web): fix get resource doc (#925)

--- src/dashboard/apigateway/apigateway/apis/web/docs/gateway/resource/doc/views.py
+++ src/dashboard/apigateway/apigateway/apis/web/docs/gateway/resource/doc/views.py
@@ -54,8 +54,7 @@ class DocRetrieveApi(generics.RetrieveAPIView):
             resource_name=resource_name,
             language=ResourceDocHandler.get_doc_language(get_current_language_code()),
         )
-        # 不公开的资源，对用户来说，就是一个不存在的资源
-        if not (resource_data and resource_data.is_public and doc_data):
+        if not (resource_data and doc_data):
             raise error_codes.NOT_FOUND
 
         generator = DocGenerator(
