9ff6237b241c4dd5f5743acf7cb91b7ee44d6c5b
KDZhu <carlchu0113@gmail.com>
2024-09-12 09:36:11 +0800

    feat: 应用权限合并优化 (#895)

---
+++ src/dashboard-front/src/views/permission/app/types.ts
@@ -0,0 +1,35 @@
+interface IPermission {
+  id: number;
+  bk_app_code: string;
+  resource_id: number;
+  resource_name: string;
+  resource_path: string;
+  resource_method: string;
+  expires: string;
+  grant_dimension: string;
+  grant_type: string;
+  renewable: boolean;
+  detail?: unknown[];
+}
+
+interface IFilterValues {
+  id: number | string
+  name: string
+  values: FilterValue[]
+}
+
+type FilterValue = { id: number | string, name: string };
+
+interface IResource {
+  id: number;
+  method: string;
+  name: string;
+  path: string;
+}
+
+export {
+  IPermission,
+  IFilterValues,
+  FilterValue,
+  IResource,
+};
