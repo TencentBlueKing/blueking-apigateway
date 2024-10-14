5daf72ed3b331850e730d6ae3336b8d3fd9bbea9
K.D.Zhu <carlchu0113@gmail.com>
2024-10-14 09:55:51 +0800

    feat: 优化发布时的时间展示 (#1021)

--- src/dashboard-front/src/http/logs.ts
+++ src/dashboard-front/src/http/logs.ts
@@ -2,12 +2,35 @@ import fetch from './fetch';
 
 const { BK_DASHBOARD_URL } = window;
 
+export interface IEventTemplate {
+  description: string
+  name: string
+  step: number
+}
+
+export interface IEvent {
+  id: number;
+  release_history_id: number;
+  name: string;
+  step: number;
+  status: 'doing' | 'success' | 'failure';
+  created_time: string;
+  detail?: Record<string, any> | null;
+}
+
+export interface ILogResponse {
+  events: IEvent[]
+  events_template: IEventTemplate[]
+  status: string
+}
+
 /**
  *  查询发布事件(日志)
  * @param apigwId 网关id
- * @returns
+ * @param historyId 历史id
+ * @returns ILogResponse
  */
-export const getLogs = (apigwId: number, historyId: number) => fetch.get(`${BK_DASHBOARD_URL}/gateways/${apigwId}/releases/histories/${historyId}/events/`);
+export const getLogs = (apigwId: number, historyId: number): Promise<ILogResponse> => fetch.get(`${BK_DASHBOARD_URL}/gateways/${apigwId}/releases/histories/${historyId}/events/`);
 
 /**
  *  版本发布接口
