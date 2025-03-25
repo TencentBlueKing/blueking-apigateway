c5dfd596163648063875a129455f0a24891bf31f
F-cq <2036478450@qq.com>
2025-03-25 15:02:57 +0800

    fix(core/admin.py): fix django admin search of Context model

--- src/dashboard/apigateway/apigateway/core/admin.py
+++ src/dashboard/apigateway/apigateway/core/admin.py
@@ -18,6 +18,7 @@
 
 from django.contrib import admin
 
+from apigateway.core.constants import ContextScopeTypeEnum
 from apigateway.core.models import (
     JWT,
     Backend,
@@ -118,6 +119,33 @@ class ContextAdmin(admin.ModelAdmin):
     list_display = ["id", "scope_type", "scope_id", "type"]
     list_filter = ["scope_type", "type"]
     search_fields = ["scope_id"]
+
+    def get_search_results(self, request, queryset, search_term):
+        # 根据网关名称搜索结果，同时保留过滤器条件
+        queryset, use_distinct = super().get_search_results(request, queryset, "")
+
+        if not search_term:
+            return queryset, use_distinct
+
+        gateway_obj = Gateway.objects.filter(name=search_term).first()
+        if not gateway_obj:
+            # 没有查询到网关信息，返回空的 queryset
+            return self.model.objects.none(), use_distinct
+
+        # 查询 GATEWAY 类型的记录
+        gateway_queryset = queryset.filter(scope_type=ContextScopeTypeEnum.GATEWAY.value, scope_id=gateway_obj.id)
+
+        # 查询 STAGE 类型的记录
+        stage_ids = list(Stage.objects.filter(gateway=gateway_obj).values_list("id", flat=True))
+        stage_queryset = queryset.filter(scope_type=ContextScopeTypeEnum.STAGE.value, scope_id__in=stage_ids)
+
+        # 查询 RESOURCE 类型的记录
+        resource_ids = list(Resource.objects.filter(gateway=gateway_obj).values_list("id", flat=True))
+        resource_queryset = queryset.filter(scope_type=ContextScopeTypeEnum.RESOURCE.value, scope_id__in=resource_ids)
+
+        queryset = gateway_queryset | stage_queryset | resource_queryset
+
+        return queryset, use_distinct
 
 
 class JWTAdmin(admin.ModelAdmin):
