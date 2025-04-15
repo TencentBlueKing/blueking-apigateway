549f7616a75f94cdd76b60f1ebbde9da297ac72d
wklken <wklken@gmail.com>
2025-04-15 20:38:17 +0800

    fix(apis/open/resource_version/views.py): the sdk use the resource_version_title, so add it back

--- src/dashboard/apigateway/apigateway/apis/open/resource_version/views.py
+++ src/dashboard/apigateway/apigateway/apis/open/resource_version/views.py
@@ -134,6 +134,7 @@ class ResourceVersionReleaseApi(generics.CreateAPIView):
             "OK",
             data={
                 "version": resource_version["version"],
+                "resource_version_title": "",
                 "stage_names": list(Stage.objects.filter(id__in=stage_ids).values_list("name", flat=True)),
             },
         )
