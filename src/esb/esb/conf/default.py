ffd6cb4249d433df0e3143e3e855998e761b11e7
wklken <wklken@gmail.com>
2025-04-23 11:53:43 +0800

    esb/conf/default.py: fix keyerror (#1288)

--- src/esb/esb/conf/default.py
+++ src/esb/esb/conf/default.py
@@ -176,6 +176,9 @@ if BK_ESB_DATABASE_TLS_ENABLED:
         default_ssl_options["cert"] = default_cert_file
         default_ssl_options["key"] = default_key_file
 
+    if "OPTIONS" not in DATABASES["default"]:
+        DATABASES["default"]["OPTIONS"] = {}
+
     DATABASES["default"]["OPTIONS"]["ssl"] = default_ssl_options
 
 
