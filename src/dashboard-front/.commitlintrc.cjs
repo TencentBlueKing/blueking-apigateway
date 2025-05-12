1011ad9e8120e9f3f74b5de316f0064b5da7b42f
KDZhu <carlchu0113@gmail.com>
2025-05-12 14:19:32 +0800

    feat(deps): commit lint

---
+++ src/dashboard-front/.commitlintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  ignores: [commit => commit.includes('init')],
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'perf',
+        'style',
+        'docs',
+        'test',
+        'refactor',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+        'wip',
+        'workflow',
+        'types',
+      ],
+    ],
+  },
+};
