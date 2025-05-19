c06efd268d5b206e43ba636065e4254644d1ffa2
wklken <wklken@gmail.com>
2025-05-19 20:01:11 +0800

    feat(apis/web/mcp_server): add apis

---
+++ src/dashboard/apigateway/apigateway/apis/web/mcp_server/serializers.py
@@ -0,0 +1,80 @@
+#
+# TencentBlueKing is pleased to support the open source community by making
+# 蓝鲸智云 - API 网关(BlueKing - APIGateway) available.
+# Copyright (C) 2017 THL A29 Limited, a Tencent company. All rights reserved.
+# Licensed under the MIT License (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
+#
+#     http://opensource.org/licenses/MIT
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# We undertake not to change the open source license (MIT license) applicable
+# to the current version of the project delivered to anyone in the future.
+#
+
+from typing import Any, Dict
+
+from rest_framework import serializers
+
+from apigateway.apps.mcp_server.constants import MCPServerStatusEnum
+from apigateway.apps.mcp_server.models import MCPServer
+
+
+class MCPServerCreateInputSLZ(serializers.ModelSerializer):
+    class Meta:
+        model = MCPServer
+        fields = ("name", "description", "stage", "is_public", "labels", "resource_ids")
+        lookup_field = "id"
+
+
+class MCPServerBaseOutputSLZ(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True, help_text="MCPServer ID")
+    name = serializers.CharField(read_only=True, help_text="MCPServer 名称")
+    description = serializers.CharField(read_only=True, help_text="MCPServer 描述")
+
+    is_public = serializers.BooleanField(read_only=True, help_text="MCPServer 是否公开")
+
+    labels = serializers.ListField(read_only=True, help_text="MCPServer 标签")
+    resource_ids = serializers.ListField(read_only=True, help_text="MCPServer 资源 ID")
+
+    status = serializers.ChoiceField(
+        read_only=True, help_text="MCPServer 状态", choices=MCPServerStatusEnum.get_choices()
+    )
+
+    stage = serializers.SerializerMethodField(help_text="MCPServer 环境")
+
+    def get_stage(self, obj) -> Dict[str, Any]:
+        return self.context["stages"][obj.stage.id]
+
+
+class MCPServerListOutputSLZ(MCPServerBaseOutputSLZ):
+    pass
+
+
+class MCPServerRetrieveOutputSLZ(MCPServerBaseOutputSLZ):
+    pass
+
+
+class MCPServerUpdateInputSLZ(serializers.ModelSerializer):
+    class Meta:
+        model = MCPServer
+        fields = ("description", "is_public", "labels", "resource_ids")
+        lookup_field = "id"
+
+
+class MCPServerUpdateStatusInputSLZ(serializers.ModelSerializer):
+    class Meta:
+        model = MCPServer
+        fields = ("status",)
+        lookup_field = "id"
+
+
+class MCPServerUpdateLabelsInputSLZ(serializers.ModelSerializer):
+    class Meta:
+        model = MCPServer
+        fields = ("labels",)
+        lookup_field = "id"
