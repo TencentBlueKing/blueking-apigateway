6600380505a349b7987c2b8f4b160b2860590613
F-cq <2036478450@qq.com>
2025-05-20 17:13:10 +0800

    feat(apps/mcp_server): add mcp_server app permission model

--- src/dashboard/apigateway/apigateway/apps/mcp_server/constants.py
+++ src/dashboard/apigateway/apigateway/apps/mcp_server/constants.py
@@ -17,8 +17,26 @@
 #
 
 from blue_krill.data_types.enum import EnumField, StructuredEnum
+from django.utils.translation import gettext_lazy as _
 
 
 class MCPServerStatusEnum(StructuredEnum):
     INACTIVE = EnumField(0, "已停用")
     ACTIVE = EnumField(1, "启用中")
+
+
+class McpServerAppPermissionApplyExpireDaysEnum(StructuredEnum):
+    FOREVER = EnumField(0, label=_("永久"))
+
+
+class GrantTypeEnum(StructuredEnum):
+    # 初始化方式主动授权
+    INITIALIZE = EnumField("initialize", label=_("主动授权"))
+    # 申请审批方式授权
+    APPLY = EnumField("apply", label=_("申请审批"))
+
+
+class McpServerAppPermissionApplyStatusEnum(StructuredEnum):
+    APPROVED = EnumField("approved", label=_("通过"))
+    REJECTED = EnumField("rejected", label=_("驳回"))
+    PENDING = EnumField("pending", label=_("待审批"))
