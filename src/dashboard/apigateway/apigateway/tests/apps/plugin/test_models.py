e4ce1b94a05acaf55968901b0b6f4c0de6ed0925
wklken <wklken@gmail.com>
2025-07-08 15:48:48 +0800

    docs(license): update header (#1604)

--- src/dashboard/apigateway/apigateway/tests/apps/plugin/test_models.py
+++ src/dashboard/apigateway/apigateway/tests/apps/plugin/test_models.py
@@ -1,7 +1,7 @@
 #
 # TencentBlueKing is pleased to support the open source community by making
 # 蓝鲸智云 - API 网关(BlueKing - APIGateway) available.
-# Copyright (C) 2017 THL A29 Limited, a Tencent company. All rights reserved.
+# Copyright (C) 2025 Tencent. All rights reserved.
 # Licensed under the MIT License (the "License"); you may not use this file except
 # in compliance with the License. You may obtain a copy of the License at
 #
