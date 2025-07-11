7b9c59cd69a1f272419bf49519ab9b112d741cb3
hanyajun <1581532052@qq.com>
2025-07-11 16:31:32 +0800

    fix: fix lint (#1628)

--- src/core-api/pkg/database/dao/app_gateway_permission.go
+++ src/core-api/pkg/database/dao/app_gateway_permission.go
@@ -16,6 +16,7 @@
  * to the current version of the project delivered to anyone in the future.
  */
 
+// Package dao ...
 package dao
 
 //go:generate mockgen -source=$GOFILE -destination=./mock/$GOFILE -package=mock
