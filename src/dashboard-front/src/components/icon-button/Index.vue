bd1bf091f46977f3068c446bc4dd7f11fc63f453
K.D.Zhu <carlchu0113@gmail.com>
2025-07-11 10:44:38 +0800

    feat(icon): 公用图标和图标button (#1621)
    # Reviewed, transaction id: 49918

---
+++ src/dashboard-front/src/components/icon-button/Index.vue
@@ -0,0 +1,28 @@
+<template>
+  <BkButton
+    v-bind="$attrs"
+  >
+    <AgIcon
+      v-if="icon"
+      :name="icon"
+      :size="size"
+      :svg="svg"
+      class="mr-8px"
+    />
+    <span>
+      <slot />
+    </span>
+  </BkButton>
+</template>
+
+<script setup lang="ts">
+import AgIcon from '@/components/ag-icon/Index.vue';
+
+interface IProps {
+  icon?: string
+  size?: string
+  svg?: boolean
+}
+
+const { icon = '', size = '14', svg = false } = defineProps<IProps>();
+</script>
--- src/dashboard-front/src/main.ts
+++ src/dashboard-front/src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
-import App from './app.vue';
+import App from './App.vue';
 import router from './router';
 import 'virtual:uno.css';
 import '@unocss/reset/normalize.css';
@@ -10,6 +10,8 @@ import '@unocss/reset/normalize.css';
 import bkui from 'bkui-vue';
 // 全量引入 bkui-vue 样式
 import '../node_modules/bkui-vue/dist/style.variable.css';
+// icon font
+import '@/lib/bk_icon_font/style.css';
 
 import i18n from './locales';
 import VueDOMPurifyHTML from 'vue-dompurify-html';
