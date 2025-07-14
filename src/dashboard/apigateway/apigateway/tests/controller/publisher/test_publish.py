f1e9916bec8d719602091b1098bdeb7083bd199e
F-cq <2036478450@qq.com>
2025-07-14 14:07:40 +0800

    fix(controller/publisher): 修复发布校验问题

--- src/dashboard/apigateway/apigateway/tests/controller/publisher/test_publish.py
+++ src/dashboard/apigateway/apigateway/tests/controller/publisher/test_publish.py
@@ -69,6 +69,12 @@ class TestTriggerGatewayPublish:
         assert ok is False
         assert f"The data structure of version [{fake_release.resource_version.object_display}] is incompatible" in msg
 
+    def test__is_gateway_ok_for_releasing_with_skip_stage_disable_check(self, fake_stage, fake_release):
+        source = PublishSourceEnum.STAGE_DISABLE
+        ok, msg = _is_gateway_ok_for_releasing(fake_release, source)
+        assert ok is True
+        assert msg == ""
+
     def test__save_release_history(self, fake_release):
         source = PublishSourceEnum.BACKEND_UPDATE
         release_history = _save_release_history(fake_release, source, "test")
