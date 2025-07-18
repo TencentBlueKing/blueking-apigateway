8b2dc75f9c794461449cd4048414fe9a3bf0b295
lhzzforever <zly2576885332@163.com>
2025-07-18 21:19:30 +0800

    feature: 操作记录模块重构 --story=125692338
    feature: 添加漏缺的前端codecc签名
    
    fix: useMaxTableLimit设置默认占位像素

--- src/dashboard-front/src/views/audit-log/routes.ts
+++ src/dashboard-front/src/views/audit-log/routes.ts
@@ -15,38 +15,21 @@
  * We undertake not to change the open source license (MIT license) applicable
  * to the current version of the project delivered to anyone in the future.
  */
-import { Message } from 'bkui-vue';
+import type { RouteRecordRaw } from 'vue-router';
 import { t } from '@/locales';
 
-export function copy(value: string) {
-  if (!value) {
-    Message({
-      theme: 'warning',
-      message: t('暂无可复制数据'),
-      delay: 2000,
-      dismissable: false,
-    });
-    return;
-  }
-  const el = document.createElement('textarea');
-  el.value = value;
-  el.setAttribute('readonly', '');
-  el.style.position = 'absolute';
-  el.style.left = '-9999px';
-  document.body.appendChild(el);
-  const selected = document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false;
-  el.select();
-  document.execCommand('copy');
-  document.body.removeChild(el);
-  if (selected) {
-    document.getSelection().removeAllRanges();
-    document.getSelection().addRange(selected);
-  }
-  Message({
-    theme: 'success',
-    width: 'auto',
-    message: t('复制成功'),
-    delay: 2000,
-    dismissable: false,
-  });
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/:id/audit',
+    name: 'AuditLog',
+    component: () => import('@/views/audit-log/Index.vue'),
+    meta: {
+      title: t('操作记录'),
+      matchRoute: 'AuditLog',
+    },
+  },
+];
+
+export default function getAuditLogRoutes() {
+  return routes;
 }
