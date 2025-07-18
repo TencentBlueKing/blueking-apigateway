7fceaed2a6b8df1090586914eeeeb500f8b9c2b8
lhzzforever <zly2576885332@163.com>
2025-07-18 21:41:05 +0800

    feature: 添加漏缺的前端codecc签名

--- src/dashboard-front/src/views/audit-log/Index.vue
+++ src/dashboard-front/src/views/audit-log/Index.vue
@@ -1,3 +1,20 @@
+/*
+ * TencentBlueKing is pleased to support the open source community by making
+ * 蓝鲸智云 - API 网关(BlueKing - APIGateway) available.
+ * Copyright (C) 2025 Tencent. All rights reserved.
+ * Licensed under the MIT License (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * We undertake not to change the open source license (MIT license) applicable
+ * to the current version of the project delivered to anyone in the future.
+ */
 <template>
   <div class="page-wrapper-padding audit-log-content">
     <div class="ag-top-header">
@@ -122,13 +139,13 @@ const defaultSearchData = ref<IAuditLog>({
   time_end: '',
   order_by: '',
 });
-const defaultFilterData = ref<IAuditLog>({
+const defaultFilterData = ref<Record<string, string>>({
   op_type: 'ALL',
   op_object_type: 'ALL',
   op_status: 'ALL',
 });
-const curSelectData = ref<{ [key: string]: any }>(cloneDeep(defaultFilterData));
-const filterData = ref<{ [key: string]: any }>(cloneDeep(defaultSearchData));
+const curSelectData = ref<Record<string, string>>(cloneDeep(defaultFilterData));
+const filterData = ref<IAuditLog>(cloneDeep(defaultSearchData));
 const OperateRecordObjectType = ref(
   accessLogStore.auditOptions.OPObjectType.map((item: Record<string, string>) => {
     return {
