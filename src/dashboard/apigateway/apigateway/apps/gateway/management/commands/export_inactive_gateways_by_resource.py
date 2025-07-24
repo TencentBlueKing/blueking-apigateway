ee3e886baba6dd15fc742b65f637658b822b136d
F-cq <2036478450@qq.com>
2025-07-24 16:28:24 +0800

    feat(apps/gateway/commands): add export_inactive_gateways_by_resource

---
+++ src/dashboard/apigateway/apigateway/apps/gateway/management/commands/export_inactive_gateways_by_resource.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+#
+# TencentBlueKing is pleased to support the open source community by making
+# 蓝鲸智云 - API 网关(BlueKing - APIGateway) available.
+# Copyright (C) 2025 Tencent. All rights reserved.
+# Licensed under the MIT License (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
+#
+#     http://opensource.org/licenses/MIT
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# We undertake not to change the open source license (MIT license) applicable
+# to the current version of the project delivered to anyone in the future.
+#
+import csv
+import datetime
+
+from django.core.management.base import BaseCommand
+from django.db.models import Count
+from django.utils import timezone
+from django.utils.translation import gettext as _
+
+from apigateway.apps.metrics.models import StatisticsGatewayRequestByDay
+from apigateway.core.constants import StageStatusEnum
+from apigateway.core.models import Gateway, ReleasedResource, Stage
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--template",
+            type=str,
+            required=True,
+            help="网关访问地址",
+        )
+
+        parser.add_argument(
+            "--resource-count",
+            type=int,
+            required=True,
+            help="资源数量",
+        )
+
+        parser.add_argument(
+            "--days",
+            type=int,
+            default=365,
+            help="查询范围（单位：天）",
+        )
+
+    def handle(self, *args, **options):
+        template = options["template"]
+        days = options["days"]
+        resource_count = options["resource_count"]
+
+        gateway_ids = (
+            Stage.objects.filter(
+                status=StageStatusEnum.ACTIVE.value,
+            )
+            .values_list("gateway_id", flat=True)
+            .distinct()
+        )
+
+        released_resource_gateway_id_set = set(
+            ReleasedResource.objects.filter(gateway_id__in=gateway_ids)
+            .values("gateway_id")
+            .annotate(resource_version_count=Count("resource_version_id"))
+            .filter(resource_version_count__gte=resource_count)
+            .values_list("gateway_id", flat=True)
+        )
+
+        start_time = timezone.now() + datetime.timedelta(days=-days)
+        statistics_gateway_id_set = set(
+            StatisticsGatewayRequestByDay.objects.filter(
+                gateway_id__in=released_resource_gateway_id_set,
+                start_time__gte=start_time,
+            )
+            .values_list("gateway_id", flat=True)
+            .distinct()
+        )
+
+        export_gateway_ids = released_resource_gateway_id_set - statistics_gateway_id_set
+        data = [
+            {
+                "name": obj.name,
+                "desc": obj.description or "",
+                "access_url": template.format(gateway_id=obj.id),
+                "deactivated": "否" if obj.is_active else "是",
+                "maintainers": obj._maintainers,
+                "created_by": obj.created_by,
+                "created_time": obj.created_time,
+                "options": "",
+            }
+            for obj in Gateway.objects.filter(id__in=export_gateway_ids).order_by("name")
+        ]
+
+        if not data:
+            self.stdout.write("无数据导出")
+            return
+
+        now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        self.export_to_csv(data, f"gateway_inactive_statistics_by_resource_{days}_{now}")
+
+    def export_to_csv(self, data, filename):
+        full_path = f"{filename}.csv"
+
+        with open(full_path, "w", newline="", encoding="utf-8-sig") as csvfile:
+            headers = [
+                "name",
+                "desc",
+                "access_url",
+                "deactivated",
+                "maintainers",
+                "created_by",
+                "created_time",
+                "options",
+            ]
+            header_row = {
+                "name": _("网关名"),
+                "desc": _("网关描述"),
+                "access_url": _("网关访问地址"),
+                "deactivated": _("是否已停用"),
+                "maintainers": _("网关负责人"),
+                "created_by": _("网关创建人"),
+                "created_time": _("创建时间"),
+                "options": _("操作选项"),
+            }
+            io_csv = csv.DictWriter(csvfile, fieldnames=headers, extrasaction="ignore")
+            io_csv.writerow(header_row)
+            io_csv.writerows(data)
+
+        self.stdout.write(self.style.SUCCESS(f"已导出到 {full_path} (共 {len(data)} 条)"))
