aa2bfa289e6c9cb65cd1de29299bca66ba7b1d99
wklken <wklken@gmail.com>
2025-08-07 16:38:28 +0800

    test(controller/crds): add release_data/test_release_data.py

--- src/dashboard/apigateway/apigateway/tests/controller/crds/release_data/test_release_data.py
+++ src/dashboard/apigateway/apigateway/tests/controller/crds/release_data/test_release_data.py
@@ -21,6 +21,8 @@ from ddf import G
 
 from apigateway.apps.plugin.constants import PluginBindingScopeEnum
 from apigateway.apps.plugin.models import PluginBinding
+from apigateway.service.contexts import GatewayAuthContext
+from apigateway.service.gateway_jwt import GatewayJWTHandler
 
 
 class TestReleaseData:
@@ -33,6 +35,17 @@ class TestReleaseData:
 
     def test_stage(self, edge_gateway_stage):
         assert self.release_data.stage == edge_gateway_stage
+
+    def test_resource_version(self, edge_resource_version):
+        assert self.release_data.resource_version == edge_resource_version
+
+    def test_jwt_private_key(self, edge_gateway):
+        jwt_private_key = GatewayJWTHandler.get_private_key(edge_gateway.pk)
+        assert self.release_data.jwt_private_key == jwt_private_key
+
+    def test_gateway_auth_config(self, edge_gateway):
+        gateway_auth_config = GatewayAuthContext().get_config(edge_gateway.pk)
+        assert self.release_data.gateway_auth_config == gateway_auth_config
 
     def test_get_stage_plugins(self, edge_gateway, edge_gateway_stage, edge_plugin_config, edge_plugin_type):
         G(
@@ -52,36 +65,3 @@ class TestReleaseData:
         plugins = self.release_data.get_stage_plugins()
         assert len(plugins) == 1
         assert plugins[0].config == {"rates": {"__default": [{"period": 60, "tokens": 100}]}}
-
-    # FIXME: it not working now
-    # def test_get_resource_plugins(self, edge_gateway, edge_resource_overwrite_stage, edge_plugin_config):
-    #     G(
-    #         PluginBinding,
-    #         gateway=edge_gateway,
-    #         scope_type=PluginBindingScopeEnum.RESOURCE.value,
-    #         scope_id=edge_resource_overwrite_stage.pk,
-    #         config=edge_plugin_config,
-    #     )
-
-    #     plugins = self.release_data.get_resource_plugins(edge_resource_overwrite_stage.pk)
-    #     assert len(plugins) == 1
-
-    # FIXME: it not working now
-    # def test_get_resource_plugins__duplicate(
-    #     self, edge_gateway, edge_resource_overwrite_stage, edge_plugin_config, edge_plugin_type
-    # ):
-    #     # 资源同时绑定了频率控制访问策略和插件
-    #     edge_plugin_type.code = "bk-rate-limit"
-    #     edge_plugin_type.save()
-
-    #     G(
-    #         PluginBinding,
-    #         gateway=edge_gateway,
-    #         scope_type=PluginBindingScopeEnum.RESOURCE.value,
-    #         scope_id=edge_resource_overwrite_stage.pk,
-    #         config=edge_plugin_config,
-    #     )
-
-    #     plugins = self.release_data.get_resource_plugins(edge_resource_overwrite_stage.pk)
-    #     assert len(plugins) == 1
-    #     assert plugins[0].config == {"rates": {"__default": [{"period": 60, "tokens": 100}]}}
