8d56ee4bbacd2b3727141f50c52fa29c4ba5d074
wklken <wklken@gmail.com>
2025-10-14 15:43:20 +0800

    feat(dockerfile): use tencentos4 (#1880)

--- src/dashboard-front/bin/entrypoint.sh
+++ src/dashboard-front/bin/entrypoint.sh
@@ -1,9 +1,7 @@
-#!/usr/bin/with-contenv sh
+#!/bin/sh
 
 # generate the runtime-env.js
-
 echo "globalThis.runtimeEnv = { BK_DASHBOARD_URL: '$BK_DASHBOARD_URL' };" > /var/www/runtime-env.js
 
-
-
-
+# start nginx
+exec nginx -g 'daemon off;'
