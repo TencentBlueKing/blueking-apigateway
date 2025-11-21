0a30313c5f6b8a21234a24b29b3dfdd9a1f6c583
F-cq <42882578+F-cq@users.noreply.github.com>
2025-11-21 17:53:57 +0800

    fix(apis/web): 修复统计报表调用方列表过滤问题 (#2037)

--- src/dashboard/apigateway/apigateway/apis/web/metrics/views.py
+++ src/dashboard/apigateway/apigateway/apis/web/metrics/views.py
@@ -247,6 +247,7 @@ class QuerySummaryCallerListApi(generics.ListAPIView):
 
         app_codes = sorted(
             StatisticsAppRequestByDay.objects.filter(
+                gateway_id=request.gateway.id,
                 stage_name=stage_name,
                 start_time__gte=timezone.datetime.fromtimestamp(data["time_start"]),
                 end_time__lte=timezone.datetime.fromtimestamp(data["time_end"]),
