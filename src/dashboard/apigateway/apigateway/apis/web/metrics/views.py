b1989bc4577bfd266b8535de277df441e20ea95b
F-cq <42882578+F-cq@users.noreply.github.com>
2025-12-02 19:44:25 +0800

    fix(apis/web): 修复统计报表的时区查询问题 (#2081)

--- src/dashboard/apigateway/apigateway/apis/web/metrics/views.py
+++ src/dashboard/apigateway/apigateway/apis/web/metrics/views.py
@@ -249,8 +249,8 @@ class QuerySummaryCallerListApi(generics.ListAPIView):
             StatisticsAppRequestByDay.objects.filter(
                 gateway_id=request.gateway.id,
                 stage_name=stage_name,
-                start_time__gte=timezone.datetime.fromtimestamp(data["time_start"]),
-                end_time__lte=timezone.datetime.fromtimestamp(data["time_end"]),
+                start_time__gte=timezone.datetime.fromtimestamp(data["time_start"], timezone.get_current_timezone()),
+                end_time__lte=timezone.datetime.fromtimestamp(data["time_end"], timezone.get_current_timezone()),
             )
             .values_list("bk_app_code", flat=True)
             .distinct()
