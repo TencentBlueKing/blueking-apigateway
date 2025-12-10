624c34a620eeef2cf57192790c434ee41d1caad3
KDZhu <carlchu0113@gmail.com>
2025-12-10 09:50:34 +0800

    fix(front-end): fix lint errors # Reviewed, transaction id: 67477

--- src/dashboard-front/env.d.ts
+++ src/dashboard-front/env.d.ts
@@ -1,43 +1,39 @@
 /// <reference types="vite/client" />
 
-interface ImportMetaEnv {
-  readonly VITE_BK_DASHBOARD_FE_URL: string;
-}
+interface ImportMetaEnv { readonly VITE_BK_DASHBOARD_FE_URL: string }
 
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
+interface ImportMeta { readonly env: ImportMetaEnv }
 
-declare module "*.css" {
+declare module '*.css' {
   const css: string;
   export default css;
 }
 
-declare module "*.png" {
+declare module '*.png' {
   const css: string;
   export default png;
 }
 
-declare module "*.js" {
+declare module '*.js' {
   const css: string;
   export default js;
 }
 
-declare module "@blueking/notice-component";
+declare module '@blueking/notice-component';
 
-declare module "@blueking/bkui-form";
+declare module '@blueking/bkui-form';
 
-declare module "@blueking/login-modal" {
+declare module '@blueking/login-modal' {
   export function showLoginModal(params: { loginUrl: string }): void;
 }
 
-declare module "@blueking/release-note";
+declare module '@blueking/release-note';
 
 declare interface Window {
-  BKANALYSIS?: { init: (params: { siteName: string }) => void };
-  BK_DASHBOARD_URL: string;
-  BK_SITE_PATH: string;
-  BK_STATIC_URL: string;
+  BKANALYSIS?: { init: (params: { siteName: string }) => void }
+  BK_DASHBOARD_URL: string
+  BK_SITE_PATH: string
+  BK_STATIC_URL: string
 }
 
 declare global {
