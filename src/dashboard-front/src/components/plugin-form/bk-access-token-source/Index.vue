02ca3d6e2f28c329e0e3c734483a0e2a6c8a4edc
shuzhenyang <148324706+shuzhenyang@users.noreply.github.com>
2025-12-11 15:40:02 +0800

    feat(plugin-form): access-token 表单修改 (#2142)

--- src/dashboard-front/src/components/plugin-form/bk-access-token-source/Index.vue
+++ src/dashboard-front/src/components/plugin-form/bk-access-token-source/Index.vue
@@ -18,12 +18,25 @@
         />
       </BkRadioGroup>
     </BkFormItem>
+    <BkFormItem
+      property="allow_fallback"
+      :label="t('允许降级')"
+      :description="t('是否允许调用方没有使用新的认证头的情况下，不报错降级使用 X-Bkapi-Authorization 认证头')"
+    >
+      <BkSwitcher
+        v-model="form.allow_fallback"
+        theme="primary"
+      />
+    </BkFormItem>
   </BkForm>
 </template>
 
 <script setup lang="ts">
 
-interface IFormModel { source: 'bearer' | 'api_key' }
+interface IFormModel {
+  source: 'bearer' | 'api_key'
+  allow_fallback: boolean
+}
 
 interface IProps { data: IFormModel }
 
@@ -33,11 +46,15 @@ const { t } = useI18n();
 
 const formRef = ref();
 
-const form = ref<IFormModel>({ source: 'bearer' });
+const form = ref<IFormModel>({
+  source: 'bearer',
+  allow_fallback: true,
+});
 
 watch(() => data, () => {
   if (data) {
     form.value.source = data.source || 'bearer';
+    form.value.allow_fallback = data.allow_fallback || true;
   }
 }, {
   immediate: true,
