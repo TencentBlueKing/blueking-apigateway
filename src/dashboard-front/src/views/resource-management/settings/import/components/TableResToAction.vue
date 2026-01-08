f5dd2ab26e6cc5f25fea7d27d09b26c76ffb7349
K.D.Zhu <carlchu0113@gmail.com>
2026-01-08 16:46:35 +0800

    fix(front-end): 修复资源标签 & mcp 市场翻页 (#2336)

--- src/dashboard-front/src/views/resource-management/settings/import/components/TableResToAction.vue
+++ src/dashboard-front/src/views/resource-management/settings/import/components/TableResToAction.vue
@@ -164,6 +164,7 @@
       fixed="right"
       prop="act"
       width="150"
+      :show-overflow-tooltip="false"
     >
       <template #default="{ row }: { row: ILocalImportedResource }">
         <div class="flex gap-12px">
