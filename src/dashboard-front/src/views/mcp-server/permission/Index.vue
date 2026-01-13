e8aef9b1f92b062e00431e6073fbf7f3042a4489
shuzhenyang <shuzhen.yang2@gientech.com>
2026-01-13 17:42:57 +0800

    feat(menu): 导航菜单修改

--- src/dashboard-front/src/views/mcp-server/permission/Index.vue
+++ src/dashboard-front/src/views/mcp-server/permission/Index.vue
@@ -347,13 +347,17 @@ const rules = reactive({
 });
 
 const getMcpList = async () => {
-  const res = await getServers(gatewayStore.currentGateway!.id!);
+  const page = {
+    offset: 0,
+    limit: 1000,
+  };
+  const res = await getServers(gatewayStore.apigwId, page);
   mcpList.value = res.results;
 };
 getMcpList();
 
 const getApplicant = async () => {
-  const response = await getMcpPermissionsApplicant(gatewayStore.currentGateway!.id!, filterData.value.mcp_server_id);
+  const response = await getMcpPermissionsApplicant(gatewayStore.apigwId, filterData.value.mcp_server_id);
   applicantList.value = response?.applicants || [];
 };
 
@@ -452,7 +456,7 @@ const handleSubmitApprove = async () => {
 
     await approveForm.value?.validate();
     await updateMcpPermissions(
-      gatewayStore.currentGateway!.id!,
+      gatewayStore.apigwId,
       filterData.value.mcp_server_id,
       curAction.value.id,
       curAction.value,
