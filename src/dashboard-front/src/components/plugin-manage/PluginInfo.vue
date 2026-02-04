be78033afb79c4df575ccd3912c7fe59c8f967df
K.D.Zhu <carlchu0113@gmail.com>
2026-02-04 16:00:15 +0800

    fix(front-end): 无表单插件 yaml 值 (#2426)
    # Reviewed, transaction id: 73652

--- src/dashboard-front/src/components/plugin-manage/PluginInfo.vue
+++ src/dashboard-front/src/components/plugin-manage/PluginInfo.vue
@@ -495,8 +495,12 @@ const handleAdd = async () => {
       Object.assign(data, { yaml: json2Yaml(JSON.stringify(formValue)).data });
       schemaFormData.value = formValue;
     }
-    // 强制要求调用方传递 X-Bk-Username header 头 插件。没有表单，需要手动传 yaml: '{}'
-    else if (choosePlugin.value === 'bk-username-required') {
+    // 没有表单的插件，需要手动传 yaml: '{}'
+    else if ([
+      'bk-status-rewrite',
+      'bk-username-required',
+      'bk-legacy-invalid-params',
+    ].includes(choosePlugin.value)) {
       Object.assign(data, { yaml: '{}' });
     }
   }
