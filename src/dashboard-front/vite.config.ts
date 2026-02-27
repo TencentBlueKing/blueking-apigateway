7f8dd60d265f295c5c9c0da2eb0e674d13d0c28f
K.D.Zhu <carlchu0113@gmail.com>
2026-02-27 14:18:38 +0800

    chore(front-end): dev 服务器地址隐藏 & 快速拉起 dev 服务器多租户环境 (#2403)

--- src/dashboard-front/vite.config.ts
+++ src/dashboard-front/vite.config.ts
@@ -1,7 +1,7 @@
 import { URL, fileURLToPath } from 'node:url';
 import path from 'path';
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import vueDevTools from 'vite-plugin-vue-devtools';
@@ -11,62 +11,65 @@ import TurboConsole from 'unplugin-turbo-console/vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vite.dev/config/
-export default defineConfig({
-  server: {
-    host: 'dev-t.paas3-dev.bktencent.com',
-    port: 8888,
-    strictPort: true,
-    // https: true,
-    open: true,
-  },
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    server: {
+      host: env.VITE_DEV_SERVER_HOST,
+      port: 8888,
+      strictPort: true,
+      // https: true,
+      open: true,
+    },
+    plugins: [
+      vue({
+        template: {
+          compilerOptions: {
           // 多租户名称展示标签不要解析为组件
-          isCustomElement: tag => tag === 'bk-user-display-name',
+            isCustomElement: tag => tag === 'bk-user-display-name',
+          },
+        },
+      }),
+      vueJsx(),
+      vueDevTools(),
+      AutoImport({
+        imports: [
+          'vue',
+          'vue-router',
+          { 'vue-i18n': ['useI18n'] },
+        ],
+        dts: './src/types/auto-imports.d.ts',
+        viteOptimizeDeps: true,
+      }),
+      UnoCSS(),
+      TurboConsole({}),
+      basicSsl(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        // 'bkui-vue': 'bkui-vue/dist/index.esm.js',
+        'bkui-lib': 'bkui-vue/lib',
+      },
+    },
+    build: {
+      manifest: true,
+      rollupOptions: {
+        input: {
+          main: path.resolve(__dirname, 'index.html'),
+          default: path.resolve(__dirname, 'default.html'),
         },
       },
-    }),
-    vueJsx(),
-    vueDevTools(),
-    AutoImport({
-      imports: [
-        'vue',
-        'vue-router',
-        { 'vue-i18n': ['useI18n'] },
-      ],
-      dts: './src/types/auto-imports.d.ts',
-      viteOptimizeDeps: true,
-    }),
-    UnoCSS(),
-    TurboConsole({}),
-    basicSsl(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      // 'bkui-vue': 'bkui-vue/dist/index.esm.js',
-      'bkui-lib': 'bkui-vue/lib',
     },
-  },
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-        default: path.resolve(__dirname, 'default.html'),
+    experimental: {
+      renderBuiltUrl(filename, { hostType }) {
+        if (hostType === 'js') {
+          return { runtime: `window.__loadAssetsUrl__(${JSON.stringify(filename)})` };
+        }
+        else {
+          return { relative: true };
+        }
       },
     },
-  },
-  experimental: {
-    renderBuiltUrl(filename, { hostType }) {
-      if (hostType === 'js') {
-        return { runtime: `window.__loadAssetsUrl__(${JSON.stringify(filename)})` };
-      }
-      else {
-        return { relative: true };
-      }
-    },
-  },
+  };
 });
