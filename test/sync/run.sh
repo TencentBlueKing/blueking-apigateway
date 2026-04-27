0c6a548fec60b695a220cde9c657b01cd9cccb7e
Zach <cszmzh@gmail.com>
2026-04-27 11:44:39 +0800

    feat(e2e): run test, for review only

--- test/sync/run.sh
+++ test/sync/run.sh
@@ -2,18 +2,22 @@
 
 CURRENT_DIR=$(pwd)
 
+# 支持两种模式：
+#   ./run.sh zach.devcloud.woa.com  → 使用域名访问（外部/有域名解析时）
+#   ./run.sh                        → 默认使用 127.0.0.1:6006 直接访问 APISIX（服务器内部 e2e 测试）
+
 if [[ -z $1 ]]; then
-    echo "please set the arg of domain"
-    exit 1
+    BK_API_URL_TMPL="http://127.0.0.1:6006/api/{api_name}"
 else
     DOMAIN=$1
+    BK_API_URL_TMPL="http://bkapi.${DOMAIN}/api/{api_name}"
 fi
 
 # sync httpbin
 docker run --rm \
     -v $CURRENT_DIR/httpbin/:/data/ \
     -e BK_APIGW_NAME=httpbin \
-    -e BK_API_URL_TMPL="http://bkapi.${DOMAIN}/api/{api_name}" \
+    -e BK_API_URL_TMPL="$BK_API_URL_TMPL" \
     -e BK_APP_CODE=bk_apigateway \
     -e BK_APP_SECRET=358627d8-d3e8-4522-8f16-b5530776bbb8 \
     --network=host \
@@ -24,7 +28,7 @@ docker run --rm \
 docker run --rm \
     -v $CURRENT_DIR/smoke/:/data/ \
     -e BK_APIGW_NAME=smoke \
-    -e BK_API_URL_TMPL="http://bkapi.${DOMAIN}/api/{api_name}" \
+    -e BK_API_URL_TMPL="$BK_API_URL_TMPL" \
     -e BK_APP_CODE=bk_apigateway \
     -e BK_APP_SECRET=358627d8-d3e8-4522-8f16-b5530776bbb8 \
     --network=host \
@@ -34,7 +38,7 @@ docker run --rm \
 docker run \
     -v $CURRENT_DIR/smoke/:/data/ \
     -e BK_APIGW_NAME=smoke \
-    -e BK_API_URL_TMPL="http://bkapi.${DOMAIN}/api/{api_name}" \
+    -e BK_API_URL_TMPL="$BK_API_URL_TMPL" \
     -e BK_APP_CODE=bk_apigateway \
     -e BK_APP_SECRET=358627d8-d3e8-4522-8f16-b5530776bbb8 \
     --network=host \
