25fae9fa7155cf1de23080a6dd84690b373fbd57
Zach <45927088+cszmzh@users.noreply.github.com>
2026-06-23 15:24:56 +0800

    feat(app-permission): add batch delete for app permissions (#2907)
    * feat(app-permission): add batch delete for app permissions
    
    * fix: self.pk in AppResourcePermission str

--- src/dashboard/apigateway/apigateway/apis/web/permission/urls.py
+++ src/dashboard/apigateway/apigateway/apis/web/permission/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
                 path(
                     "renew/",
                     views.AppPermissionRenewApi.as_view(),
-                    name="permissions.app-resource-permissions.renew",
+                    name="permissions.app-permissions.renew",
                 ),
                 path(
                     "export/",
@@ -41,6 +41,11 @@ urlpatterns = [
                     "bk-app-codes/",
                     views.AppPermissionAppCodeListApi.as_view(),
                     name="permissions.app-permissions.get_bk_app_codes",
+                ),
+                path(
+                    "batch/",
+                    views.AppPermissionDeleteApi.as_view(),
+                    name="permissions.app-permissions.batch_destroy",
                 ),
             ]
         ),
