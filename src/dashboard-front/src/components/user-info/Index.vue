6a69c43a6e38a6d40c6326f89632d0348b4aa9be
zhukeda <carlchu0113@gmail.com>
2026-08-18 17:33:51 +0800

    fix(front-end): 修复子路径环境下再登录时url出错的问题 --story=137274913

--- src/dashboard-front/src/components/user-info/Index.vue
+++ src/dashboard-front/src/components/user-info/Index.vue
@@ -41,7 +41,9 @@ const handleBkUserClick = () => {
 };
 
 const handleLogout = () => {
-  location.href = getLoginURL(envStore.env.BK_LOGIN_URL, location.origin, 'small');
+  // 处理配置配置了子路径时的情况
+  const currentPath = (!window.BK_SITE_PATH || window.BK_SITE_PATH === '/') ? location.origin : window.BK_SITE_PATH;
+  location.href = getLoginURL(envStore.env.BK_LOGIN_URL, currentPath, 'small');
 };
 </script>
 
