1d0ea1c7678a41ad797a0b63c455bc28b67e9daa
shuzhenyang <shuzhen.yang2@gientech.com>
2026-08-25 09:47:16 +0800

    fix(resource): 修复批量修改资源后生成版本按钮状态

--- src/dashboard-front/src/views/resource-management/settings/Index.vue
+++ src/dashboard-front/src/views/resource-management/settings/Index.vue
@@ -1361,6 +1361,7 @@ const handleShowBatch = () => {
 const handleOutBatch = () => {
   showBatch.value = false;
   handleClearSelection();
+  handleShowVersion();
   exportDropData.value = [
     {
       value: 'all',
