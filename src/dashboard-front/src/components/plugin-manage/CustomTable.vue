6ce075fb81633bf2da16c7f3e47dc92f85939477
K.D.Zhu <carlchu0113@gmail.com>
2026-09-15 11:24:44 +0800

    feat(front-end): 调整插件展示表格列宽 --story=138153178 (#3285)

--- src/dashboard-front/src/components/plugin-manage/CustomTable.vue
+++ src/dashboard-front/src/components/plugin-manage/CustomTable.vue
@@ -1,33 +1,35 @@
 <template>
-  <table class="config-display-table">
-    <thead>
-      <tr>
-        <th
-          v-for="(col, index) in columns"
-          :key="index"
-          :style="{width: index === 0 ? `${col.width}px` : 'auto'}"
-          :rowspan="col.rowspan"
+  <div class="config-display-table-wrapper">
+    <table class="config-display-table">
+      <thead>
+        <tr>
+          <th
+            v-for="(col, index) in columns"
+            :key="index"
+            :style="index === 0 ? { width: firstColWidth, minWidth: firstColWidth } : undefined"
+            :rowspan="col.rowspan"
+          >
+            {{ col.label }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="row in data"
+          :key="row.key"
         >
-          {{ col.label }}
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr
-        v-for="row in data"
-        :key="row.key"
-      >
-        <td
-          v-for="(col, index) in columns"
-          :key="index"
-          :style="{width: index === 0 ? `${col.width}px` : 'auto', backgroundColor: index === 0 ? '#fafbfd' : ''}"
-          :rowspan="row.rowspan"
-        >
-          {{ renderValue(row, col) }}
-        </td>
-      </tr>
-    </tbody>
-  </table>
+          <td
+            v-for="(col, index) in columns"
+            :key="index"
+            :style="index === 0 ? { width: firstColWidth, minWidth: firstColWidth } : undefined"
+            :rowspan="row.rowspan"
+          >
+            {{ renderValue(row, col) }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -39,6 +41,15 @@ interface IProps {
 }
 
 const { columns, data } = defineProps<IProps>();
+
+// auto 使用统一的基础列宽，长字段仍可通过自动表格布局撑宽第一列。
+const firstColWidth = computed(() => {
+  const width = columns[0]?.width;
+  if (width === undefined || width === 'auto') {
+    return '200px';
+  }
+  return typeof width === 'number' || /^\d+(\.\d+)?$/.test(width) ? `${width}px` : width;
+});
 
 const renderValue = (row: IBaseTableRow, col: IColumn) => {
   // @ts-ignore
@@ -52,11 +63,17 @@ const renderValue = (row: IBaseTableRow, col: IColumn) => {
 </script>
 
 <style scoped lang="scss">
+.config-display-table-wrapper {
+  min-width: 0;
+  overflow-x: auto;
+}
+
 .config-display-table {
   width: 100%;
   font-size: 12px;
   border: 1px solid #dcdee5;
   border-collapse: collapse;
+  table-layout: auto;
 
   tr {
     height: 40px;
@@ -70,11 +87,15 @@ const renderValue = (row: IBaseTableRow, col: IColumn) => {
 
     th:first-of-type, td:first-child {
       text-align: right;
+      white-space: nowrap;
+      background-color: #fafbfd;
     }
 
     th, td {
       padding: 0 16px;
       border: 1px solid #dcdee5;
+      box-sizing: border-box;
+      overflow-wrap: anywhere;
     }
 
     td {
